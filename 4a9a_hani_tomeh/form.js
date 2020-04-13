ac1a918fd45b887f1d803a7c875632eb9f43f84f
hani4881 <58553165+hani4881@users.noreply.github.com>
2020-04-13 16:18:43 +0200

    Add files via upload

---
+++ 4a9a_hani_tomeh/form.js
@@ -0,0 +1,17 @@
+/*
+*Author: Hani Tomeh Alali
+*page:form.js
+*Description: formuiler maken voor gegevens
+
+*Revision history
+*0.1           010-04-2020        Hani Tomeh Alali         initial version 
+*/
+
+
+
+function getInputValue( ){
+    var inputVal = document.getElementById("name").value;      // Het invoerelement selecteren en de waarde ervan ophalen
+    
+    alert(inputVal);                                           // De waarde weergeven
+}
+
